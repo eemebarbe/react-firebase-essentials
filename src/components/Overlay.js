bb944e8fa39cd140fc7368f4c3601ec79e28f6dd
eemebarbe <eeme.barbe@gmail.com>
2019-04-20 00:41:18 +0800

    worked on db rules and got started on overlay animation

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -7,6 +7,16 @@ const Background = styled.div`
   height: 100%;
   width: 100%;
   background-color: ${props => props.theme.overlayBackground};
+  &::after {
+    content: "";
+    position: absolute;
+    left: calc(50% - 75px);
+    top: calc(50% - 75px);
+    border-radius: 100%;
+    width: 150px;
+    height: 150px;
+    box-shadow: 0px 0px 0px 2000px red;
+  }
 `;
 
 const Overlay = props => {
